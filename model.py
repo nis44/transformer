36876658d15a28a252c2152382ed5abfd60a7c15
Nishant <nishantgupta7995@gmail.com>
2026-04-25 16:11:43 +0530

    Implement DecoderBlock and ProjectionLayer; update Transformer class with encoding and decoding methods

--- model.py
+++ model.py
@@ -149,4 +149,90 @@ class Encoder(nn.Module):
         return self.norm(x)
     
 class DecoderBlock(nn.Module):
-     
+        
+        def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+            super().__init__()
+            self.self_attention_block = self_attention_block
+            self.cross_attention_block = cross_attention_block
+            self.feed_forward_block = feed_forward_block
+            self.residual_connections = nn.ModuleList([ResidualConnection(self_attention_block.d_model, dropout) for _ in range(3)])
+        
+        def forward(self, x, encoder_output, src_mask, trg_mask):
+            x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, trg_mask))
+            x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+            x = self.residual_connections[2](x, self.feed_forward_block)
+            return x
+        
+
+class Decoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, trg_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, trg_mask)
+        return self.norm(x) 
+
+class ProjectionLayer(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+
+        self.proj = nn.linear(d_model, vocab_size)
+    def forward(self, x):
+        return torch.log_softmax(self.proj(x), dim=-1)
+    
+
+class Transformer(nn.Module): 
+
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbeddings, trt_embed: InputEmbeddings, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
+
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.trg_embed = trt_embed
+        self.src_pos = src_pos
+        self.trg_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src.embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+    
+    def decode(self, encoder_output, src_mask, trg, trg_mask):
+        trg = self.trg_embed(trg)
+        trg = self.trg_pos(trg)
+        return self.decoder(trg, encoder_output, src_mask, trg_mask)
+    
+
+    def project(self, decoder_output):
+        return self.projection_layer(decoder_output)
+    
+
+def build_transformer(src_vocab_size: int, trg_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int = 512, N: int = 6, h: int = 8, d_ff: int = 2048, dropout: float = 0.1) -> Transformer:
+    src_embed = InputEmbeddings(d_model, src_vocab_size)
+    trg_embed = InputEmbeddings(d_model, trg_vocab_size)
+
+    src_pos = PositionalEncoding(d_model, src_seq_len, dropout)
+    trg_pos = PositionalEncoding(d_model, tgt_seq_len, dropout)
+
+    encoder_block = []
+    for _ in range(N):
+        encoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        encoder_feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        encoder_block.append(EncoderLayer(encoder_self_attention_block, encoder_feed_forward_block, dropout))
+        encoder_block.append(encoder_block)
+
+
+    decoder_block = []
+    for _ in range(N):
+        decoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        decoder_cross_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        decoder_feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        decoder_block.append(DecoderBlock(decoder_self_attention_block, decoder_cross_attention_block, decoder_feed_forward_block, dropout))
+        decoder_block.append(decoder_block)
